fe89d98512b580b6e364afea430e55a0b2025c0e
Wasim <imwasim.shaik@gmail.com>
2026-01-28 08:37:06 +0530

    vpc_peering

--- peering.tf
+++ peering.tf
@@ -38,7 +38,7 @@ resource "aws_route" "private_peering"{
 
 resource "aws_route" "default_peering"{
     count = var.is_peering_required ? 1 : 0
-    route_table_id = data.aws_route_table.maint.id
+    route_table_id = data.aws_route_table.deafult.id
     destination_cidr_block = var.vpc_cidr
     vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
