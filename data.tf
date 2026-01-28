273df61a66309541b632682d91f08488c621639b
Wasim <imwasim.shaik@gmail.com>
2026-01-28 08:16:48 +0530

    vpc_peering

--- data.tf
+++ data.tf
@@ -1,7 +1,19 @@
-data "aws_availability_zones" "available"{
-    state = "available"
+data "aws_availability_zones" "available" {
+  state = "available"
 }
 
 # output "data_az" {
 #   value = aws_availability_zones.available
 # }
+
+data "aws_vpc" " default" {
+  default = true
+}
+
+data "aws_route_table" "default" {
+  vpc_id = data.aws_vpc.default.id
+  filter {
+    name   = "association-main"
+    values = ["true"]
+  }
+}
--- peering.tf
+++ peering.tf
@@ -1,7 +1,44 @@
 resource "aws_vpc_peering_connection" "default"{
     # peer_owner_id = "id of the aws account" This is optional field
     count = var.is_peering_required ? 1 : 0
-    peer_vpc_id = data.aws_vpc.default.id
+    peer_vpc_id = data.aws_vpc.default.id # acceptor
     vpc_id = aws_vpc.main.id
 
+    accepter{
+        allow_remote_vpc_dns_resolution = true
+    }
+
+    requester{
+        allow_remote_vpc_dns_resolution = true
+    }
+
+    auto_accept = true
+    tags = merge(
+        var.vpc_tags,
+        local.common_tags,
+        {
+            Name = "${local.common_name_suffix}-default"
+        }
+    )
+}
+
+resource "aws_route" "public_peering"{
+    count = var.is_peering_required ? 1 : 0
+    route_table_id = aws_route_table.public.id
+    destination_cidr_block = data.aws_vpc.default.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
+resource "aws_route" "private_peering"{
+    count = var.is_peering_required ? 1 : 0
+    route_table_id = aws_route_table.private.id
+    destination_cidr_block = data.aws_vpc.default.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
+resource "aws_route" "default_peering"{
+    count = var.is_peering_required ? 1 : 0
+    route_table_id = data.aws_route_table.default.id
+    destination_cidr_block = var.vpc_cidr
+    vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
