a63f3ec435a996aa35b73bf9df55c730d2566bc4
Wasim <imwasim.shaik@gmail.com>
2026-01-28 08:35:41 +0530

    vpc_peering

--- data.tf
+++ data.tf
@@ -13,7 +13,7 @@ data "aws_vpc" "default" {
 data "aws_route_table" "default" {
   vpc_id = data.aws_vpc.default.id
   filter {
-    name   = "association-main"
+    name   = "association.main"
     values = ["true"]
   }
 }
